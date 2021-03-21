6dc65953fe4b0e99a3e77868578e76b46b459885
yysimple <5325809+yysimple@user.noreply.gitee.com>
2021-03-21 23:38:29 +0800

    [新增]：面试题代码

---
+++ src/main/java/com/simple/jihe/ListTest.java
@@ -0,0 +1,21 @@
+package com.simple.jihe;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 项目: mianshi-test
+ * <p>
+ * 功能描述:
+ *
+ * @author: WuChengXing
+ * @create: 2021-03-20 09:14
+ **/
+public class ListTest {
+    public static void main(String[] args) {
+        List list1 = new ArrayList();
+        List list2 = new LinkedList();
+
+    }
+}
