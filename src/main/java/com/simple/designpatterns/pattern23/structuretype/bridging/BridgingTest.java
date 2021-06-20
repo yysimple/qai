78754c3294da4f31ea6c28fe4920b5ada8e501c9
吴呈兴 <1449697757@qq.com>
2021-06-20 23:21:52 +0800

    [新增]：适配器模式

---
+++ src/main/java/com/simple/designpatterns/pattern23/structuretype/bridging/BridgingTest.java
@@ -0,0 +1,15 @@
+package com.simple.designpatterns.pattern23.structuretype.bridging;
+
+/**
+ * 项目: question-study-improve
+ * <p>
+ * 功能描述: 桥接模式
+ *
+ * @author: WuChengXing
+ * @create: 2021-06-20 13:07
+ **/
+public class BridgingTest {
+    public static void main(String[] args) {
+
+    }
+}
