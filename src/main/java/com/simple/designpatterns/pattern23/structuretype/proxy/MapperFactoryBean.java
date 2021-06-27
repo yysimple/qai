140e08d9f499c332eb2998e53baed304a17573ab
吴呈兴 <1449697757@qq.com>
2021-06-27 15:10:55 +0800

    [新增]：代理模式：注释

--- src/main/java/com/simple/designpatterns/pattern23/structuretype/proxy/MapperFactoryBean.java
+++ src/main/java/com/simple/designpatterns/pattern23/structuretype/proxy/MapperFactoryBean.java
@@ -16,14 +16,18 @@ import java.lang.reflect.Proxy;
 @Slf4j
 public class MapperFactoryBean<T> implements FactoryBean<T> {
 
-    private MapperInvocationHandler mapperInvocationHandler;
-
     private Class<T> mapperInterface;
 
     public MapperFactoryBean(Class<T> mapperInterface) {
         this.mapperInterface = mapperInterface;
     }
 
+    /**
+     * 这是生成代理类的过程，针对于 UserMapper，只是一个接口，然后生成其代理类去完成真正的逻辑（MapperInvocationHandler）
+     * 然后将这个生成的代理类注入到spring容器之中，便可以使用了
+     * @return
+     * @throws Exception
+     */
     @Override
     public T getObject() throws Exception {
         return (T) Proxy.newProxyInstance(this.getClass().getClassLoader(), new Class[]{mapperInterface}, new MapperInvocationHandler());
