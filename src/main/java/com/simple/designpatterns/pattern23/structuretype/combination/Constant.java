f14714e7f267678a53009dd35241fd41e9c452a5
吴呈兴 <1449697757@qq.com>
2021-07-14 22:32:22 +0800

    [新增]：组合模式修改

--- src/main/java/com/simple/designpatterns/pattern23/structuretype/combination/Constant.java
+++ src/main/java/com/simple/designpatterns/pattern23/structuretype/combination/Constant.java
@@ -18,29 +18,4 @@ public final class Constant {
      * 果实节点
      */
     public static final Integer NODE_TYPE_FRUIT = 2;
-
-    /**
-     * 等于
-     */
-    public static final Integer EQUAL = 1;
-
-    /**
-     * 大于
-     */
-    public static final Integer GRANT = 2;
-
-    /**
-     * 小于
-     */
-    public static final Integer LESS = 3;
-
-    /**
-     * 大于等于
-     */
-    public static final Integer GRANT_EQUAL = 4;
-
-    /**
-     * 小于等于
-     */
-    public static final Integer LESS_EQUAL = 5;
 }
