ae15fb98222905c7e1bcd3183878455ec8b9eb5c
吴呈兴 <1449697757@qq.com>
2021-07-18 15:46:03 +0800

    [新增]：命令模式修改

--- src/main/java/com/simple/designpatterns/pattern23/behavioral/command/CommandTest.java
+++ src/main/java/com/simple/designpatterns/pattern23/behavioral/command/CommandTest.java
@@ -15,12 +15,21 @@ import com.simple.designpatterns.pattern23.behavioral.command.impl.JiangXiCuisin
  **/
 public class CommandTest {
     public static void main(String[] args) {
+        /**
+         * 可以理解为guangDongCuisine、jiangXiCuisine 这两东西就是两条指令
+         */
         ICuisine guangDongCuisine = new GuangDongCuisine(new GuangDongCook());
         ICuisine jiangXiCuisine = new JiangXiCuisine(new JiangXiCook());
 
         XiaoEr xiaoEr = new XiaoEr();
+        /**
+         * 这里理解为是一个批处理脚本，相当于 linux中的 sh文件
+         */
         xiaoEr.addCuisine(guangDongCuisine);
         xiaoEr.addCuisine(jiangXiCuisine);
+        /**
+         * 这里就是命令执行，可以将其等价为 ./xxx.sh,然后就会去执行相应的可执行脚本，就会去执行那些命令
+         */
         xiaoEr.placeOrder();
     }
 }
--- src/main/java/com/simple/designpatterns/pattern23/behavioral/command/XiaoEr.java
+++ src/main/java/com/simple/designpatterns/pattern23/behavioral/command/XiaoEr.java
@@ -17,10 +17,17 @@ import java.util.List;
 public class XiaoEr {
     private List<ICuisine> cuisines = new ArrayList<>();
 
+    /**
+     * 这里相当于将命令放到sh文件中，用于批量执行
+     * @param cuisine
+     */
     public void addCuisine(ICuisine cuisine) {
         cuisines.add(cuisine);
     }
 
+    /**
+     * 这里可以理解为文件的执行命令，启动脚本等、如 .sh或者.exe
+     */
     public synchronized void placeOrder(){
         for (ICuisine cuisine : cuisines) {
             cuisine.getCuisine();
