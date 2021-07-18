6a4965a51a2cd9a949b6b5e36dafc28fa4829526
吴呈兴 <1449697757@qq.com>
2021-07-18 16:00:30 +0800

    [新增]：解释器模式

--- src/main/java/com/simple/designpatterns/pattern23/behavioral/interpreter/SQLInterpreter.java
+++ src/main/java/com/simple/designpatterns/pattern23/behavioral/interpreter/SQLInterpreter.java
@@ -15,6 +15,11 @@ public class SQLInterpreter implements SQLExpression {
         this.sqlExpr = sqlExpr;
     }
 
+    /**
+     * 这里可以理解为，通过固定的sqlExpr，可以判断 传进来的sql是查询语句还是更新语句
+     * @param context
+     * @return
+     */
     @Override
     public boolean interpret(String context) {
         if (context.contains(sqlExpr)) {
