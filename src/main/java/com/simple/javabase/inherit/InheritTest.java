54f1b65f38309b6c765429d1a31e522d2cd46fc3
yysimple <1449697757@qq.com>
2021-07-22 18:25:36 +0800

    [新增]：继承接收的测试

---
+++ src/main/java/com/simple/javabase/inherit/InheritTest.java
@@ -0,0 +1,8 @@
+package com.simple.javabase.inherit;
+
+/**
+ * @author WuChengXing
+ * @date 2021/7/22
+ */
+public class InheritTest {
+}
---
+++ src/main/java/com/simple/javabase/inherit/User.java
@@ -0,0 +1,9 @@
+package com.simple.javabase.inherit;
+
+/**
+ * @author WuChengXing
+ * @date 2021/7/22
+ */
+public class User {
+
+}
