1c026758cd255f6579a0a672ed24425b2067b0e6
wuchengxing <1449697757@qq.com>
2021-10-15 15:33:24 +0800

    [新增]: 测试Queue的使用方式

--- algorithm/src/main/java/com/simple/algorithm/labuladong/chapter00/twotree/TwoTreeMinDepthTest.java
+++ algorithm/src/main/java/com/simple/algorithm/labuladong/chapter00/twotree/TwoTreeMinDepthTest.java
@@ -1,9 +1,63 @@
 package com.simple.algorithm.labuladong.chapter00.twotree;
 
+import com.simple.algorithm.labuladong.common.TreeNode;
+import com.simple.algorithm.test.BaseTest;
+import lombok.Data;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author WuChengXing
  * @date 2021/10/15
  */
-public class TwoTreeMinDepthTest {
+public class TwoTreeMinDepthTest extends BaseTest {
+    @Test
+    public void testMinDepth() {
+        int i = minDepth(init());
+        System.out.println("最小深度：" + i);
+    }
 
+    /**
+     * 最小深度
+     *
+     * @param root
+     * @return
+     */
+    public int minDepth(TreeNode<Integer> root) {
+        if (root == null) return 0;
+        Queue<TreeNode<Integer>> q = new LinkedList<>();
+        q.offer(root);
+        // root 本身就是一层，depth 初始化为 1
+        int depth = 1;
+
+        while (!q.isEmpty()) {
+            int sz = q.size();
+            /* 将当前队列中的所有节点向四周扩散 */
+            for (int i = 0; i < sz; i++) {
+                TreeNode<Integer> cur = q.poll();
+                /* 判断是否到达终点 */
+                if (cur.getLeft() == null && cur.getRight() == null)
+                    return depth;
+                /* 将 cur 的相邻节点加入队列 */
+                if (cur.getLeft() != null)
+                    q.offer(cur.getLeft());
+                if (cur.getRight() != null)
+                    q.offer(cur.getRight());
+            }
+            /* 这里增加步数 */
+            depth++;
+        }
+        return depth;
+    }
+
+    public TreeNode<Integer> init() {
+        TreeNode<Integer> node15 = new TreeNode<>(null, null, 15);
+        TreeNode<Integer> node7 = new TreeNode<>(null, null, 7);
+        TreeNode<Integer> node20 = new TreeNode<>(node15, node7, 20);
+        TreeNode<Integer> node9 = new TreeNode<>(null, null, 9);
+        TreeNode<Integer> root = new TreeNode<>(node9, node20, 3);
+        return root;
+    }
 }
---
+++ algorithm/src/main/java/com/simple/algorithm/labuladong/common/QueueTest.java
@@ -0,0 +1,47 @@
+package com.simple.algorithm.labuladong.common;
+
+import com.simple.algorithm.test.BaseTest;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author WuChengXing
+ * @date 2021/10/15
+ */
+public class QueueTest extends BaseTest {
+
+    /**
+     * add方法，在不违背队列的容量限制的情况，往队列中添加一个元素， 如果添加成功则返回true,  如果因为容量
+     * * 限制添加失败了，则抛出IllegalStateException异常
+     * *
+     * *  在有容量限制的队列中，最好使用offer方法
+     * *  另外，add方法相对与offer方法而言，有一个专门抛出的异常IllegalStateException，代表由于容量限制，
+     * * 导致不能添加元素的异常，接口中可能看不出，等看具体实现就明白了
+     */
+    @Test
+    public void testQueueAdd() {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        queue.add(2);
+        System.out.println(queue);
+    }
+
+    /**
+     * offer方法在不违背容量限制的情况，往队列中添加一个元素，如果添加元素成功，返回true，
+     * *  如果因为空间限制，无法添加元素则，返回false；
+     * *
+     * *  在有容量限制的队列中，这个offer方法优于add方法，结合上文中add的注释，可知，
+     * *  在容量限制的队列中， add方法通过抛异常的方式表示容量已满，offer方法通过返回
+     * *  false的方式表示容量已满，抛异常处理更加耗时，offer直接返回false的方式更好
+     */
+    @Test
+    public void testQueueOffer() {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        queue.add(2);
+        queue.offer(3);
+        System.out.println(queue);
+    }
+}
