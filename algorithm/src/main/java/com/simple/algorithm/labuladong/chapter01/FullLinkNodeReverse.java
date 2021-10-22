9f3586b3d5804dd34b1d59f535ac58b3142e00a1
wuchengxing <1449697757@qq.com>
2021-10-22 10:07:56 +0800

    [新增]: 全链表反转

--- algorithm/src/main/java/com/simple/algorithm/labuladong/chapter01/FullLinkNodeReverse.java
+++ algorithm/src/main/java/com/simple/algorithm/labuladong/chapter01/FullLinkNodeReverse.java
@@ -12,11 +12,18 @@ import org.junit.Test;
 public class FullLinkNodeReverse extends BaseTest {
 
     @Test
-    public void test(){
+    public void test() {
         LinkNode<Integer> linkNode = NodeInit.initNode();
+        System.out.println(LinkNode.printNode(reverse(linkNode)));
     }
 
-    public LinkNode<Integer> reverse(LinkNode<Integer> ){
-
+    public LinkNode<Integer> reverse(LinkNode<Integer> head) {
+        if (head.next == null) {
+            return head;
+        }
+        LinkNode<Integer> last = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
     }
 }
