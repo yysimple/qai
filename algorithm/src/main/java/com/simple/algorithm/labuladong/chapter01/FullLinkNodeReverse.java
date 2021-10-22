c6443096153c2548d9ec1afcc9df0739de7e8388
wuchengxing <1449697757@qq.com>
2021-10-22 14:24:37 +0800

    [新增]: 全链表反转的理解

--- algorithm/src/main/java/com/simple/algorithm/labuladong/chapter01/FullLinkNodeReverse.java
+++ algorithm/src/main/java/com/simple/algorithm/labuladong/chapter01/FullLinkNodeReverse.java
@@ -18,12 +18,17 @@ public class FullLinkNodeReverse extends BaseTest {
     }
 
     public LinkNode<Integer> reverse(LinkNode<Integer> head) {
+        // 以头结点的下一个节点为遍历目标，当下一个节点没有值的时候，也即head已经来到尾部了
         if (head.next == null) {
             return head;
         }
+        // 这里已经拿到了反转后的最后一个节点
         LinkNode<Integer> last = reverse(head.next);
+        // 可以理解这里为交换位置，其实就是反转
         head.next.next = head;
+        // 最后将头节点的下一个节点置空
         head.next = null;
+        // 返回最后一个节点，也即是新的头结点
         return last;
     }
 }
---
+++ algorithm/src/main/java/com/simple/algorithm/labuladong/chapter01/ReverseNNode.java
@@ -0,0 +1,19 @@
+package com.simple.algorithm.labuladong.chapter01;
+
+import org.junit.Test;
+
+/**
+ * @author WuChengXing
+ * @date 2021/10/22
+ */
+public class ReverseNNode {
+
+    @Test
+    public void test() {
+
+    }
+
+    public void ReverseNNode() {
+
+    }
+}
