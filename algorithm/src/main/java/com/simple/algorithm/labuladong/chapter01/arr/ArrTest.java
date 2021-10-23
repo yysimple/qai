157c36b75978636883f640211ad7d69971965328
wuchengxing <1449697757@qq.com>
2021-10-23 09:57:16 +0800

    [新增]：新增算法基本框架

---
+++ algorithm/src/main/java/com/simple/algorithm/labuladong/chapter01/arr/ArrTest.java
@@ -0,0 +1,10 @@
+package com.simple.algorithm.labuladong.chapter01.arr;
+
+/**
+ * 功能描述:
+ *
+ * @author: WuChengXing
+ * @create: 2021-10-23 09:56
+ **/
+public class ArrTest {
+}
---
+++ algorithm/src/main/java/com/simple/algorithm/labuladong/chapter01/figure/FigureTest.java
@@ -0,0 +1,10 @@
+package com.simple.algorithm.labuladong.chapter01.figure;
+
+/**
+ * 功能描述:
+ *
+ * @author: WuChengXing
+ * @create: 2021-10-23 09:56
+ **/
+public class FigureTest {
+}
