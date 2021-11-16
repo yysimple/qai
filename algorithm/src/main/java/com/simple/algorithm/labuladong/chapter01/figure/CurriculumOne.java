bcf833b34b37440269cb69a7bc1a96b1cbe000b7
wuchengxing <1449697757@qq.com>
2021-11-17 00:36:06 +0800

    [新增]：课程表1

--- algorithm/src/main/java/com/simple/algorithm/labuladong/chapter01/figure/CurriculumOne.java
+++ algorithm/src/main/java/com/simple/algorithm/labuladong/chapter01/figure/CurriculumOne.java
@@ -20,12 +20,44 @@ public class CurriculumOne extends BaseTest {
 
     @Test
     public void test() {
-
+        int curriculums = 2;
+        int[][] prerequisites = new int[4][4];
+        prerequisites[0] = new int[]{1, 0};
+        prerequisites[1] = new int[]{0, 1};
+        System.out.println(canFinish(curriculums, prerequisites));
     }
 
     public Boolean canFinish(int curriculums, int[][] prerequisites) {
+        // 构建图
         List<Integer>[] graph = buildGraph(curriculums, prerequisites);
+        // 记录遍历过的节点，防止走回头路
+        visited = new boolean[curriculums];
+        // 记录一次 traverse 递归经过的节点
+        alreadyPath = new boolean[curriculums];
+        for (int i = 0; i < curriculums; i++) {
+            // 先从课程遍历，里层会去递归能够到达的路径
+            traverse(graph, i);
+        }
         return !hasCycle;
+    }
+
+    public void traverse(List<Integer>[] graph, int node) {
+        // 已经在遍历路径上的节点，证明已经出现了，所以有环
+        if (alreadyPath[node]) {
+            hasCycle = true;
+        }
+        // 有环或者已经遍历了，则跳出循环
+        if (visited[node] || hasCycle) {
+            return;
+        }
+        visited[node] = true;
+        alreadyPath[node] = true;
+        for (Integer integer : graph[node]) {
+            traverse(graph, integer);
+        }
+        // 回撤
+        alreadyPath[node] = false;
+
     }
 
     public List<Integer>[] buildGraph(int curriculums, int[][] prerequisites) {
