c823304c889cd316951751b5141805f128ae6a97
wuchengxing <1449697757@qq.com>
2021-11-24 00:21:07 +0800

    [新增]：寻找名人

---
+++ algorithm/src/main/java/com/simple/algorithm/labuladong/chapter01/figure/FindPeople.java
@@ -0,0 +1,18 @@
+package com.simple.algorithm.labuladong.chapter01.figure;
+
+import org.junit.Test;
+
+/**
+ * 功能描述:
+ *
+ * @author: WuChengXing
+ * @create: 2021-11-24 00:09
+ **/
+public class FindPeople {
+
+    @Test
+    public void test(){
+
+    }
+
+}
