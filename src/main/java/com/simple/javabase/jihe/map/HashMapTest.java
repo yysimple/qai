921699215cddb3b909de5e996888542e5469eff3
wuchengxing <1449697757@qq.com>
2021-12-08 13:47:02 +0800

    [新增]：Map学习

---
+++ src/main/java/com/simple/javabase/jihe/map/HashMapTest.java
@@ -0,0 +1,16 @@
+package com.simple.javabase.jihe.map;
+
+import java.util.HashMap;
+
+/**
+ * 功能描述:
+ *
+ * @author: WuChengXing
+ * @create: 2021-12-07 22:02
+ **/
+public class HashMapTest {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("1", "1");
+    }
+}
