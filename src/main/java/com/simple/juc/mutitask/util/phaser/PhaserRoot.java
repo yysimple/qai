9a9edb525d892852ac9859cb3c98fbddaf05bc19
wuchengxing <1449697757@qq.com>
2021-12-10 15:18:23 +0800

    [新增]：Phaser简单使用-注释

--- src/main/java/com/simple/juc/mutitask/util/phaser/PhaserRoot.java
+++ src/main/java/com/simple/juc/mutitask/util/phaser/PhaserRoot.java
@@ -14,6 +14,15 @@ public class PhaserRoot {
     private static Phaser phaser2 = new Phaser(phaser1, 1);
     private static Phaser phaser3 = new Phaser(phaser2, 1);
 
+    /**
+     * rootPhaser，phaser1，phaser2 调用 getRegisteredParties 方法得到的值都比构造方法中设的值多 1 个，而且也没有调用 register 方法，这就是上面总结的第一点；
+     * phaser3 只有 1 个 parties，并且它自己已经 arrive 了，但它依然被阻塞在屏障处，因为它在等 phaser2 醒来，phaser2 在等 phaser1 醒来，phaser1 在等 rootPhaser 醒来，
+     * 但 rootPhaser 因为还少一个 arrive 所以醒不来，导致所有线程都被阻塞着，只需把最后注释的一行代码打开就可以了；
+     * 谁的 end 先打印出来是不确定的，因为 rootPhaser 会一下把在 phaser1，phaser2，phaser3 阻塞的线程全部唤醒。
+     *
+     * @param args
+     * @throws InterruptedException
+     */
     public static void main(String[] args) throws InterruptedException {
         new Thread(() -> {
             System.out.println("root begin");
