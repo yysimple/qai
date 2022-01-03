02bd7e4c768bcdc6ba46ff313e1be1b718f96b49
wuchengxing <1449697757@qq.com>
2022-01-03 20:03:26 +0800

    [新增]：LFU算法框架

---
+++ algorithm/src/main/java/com/simple/algorithm/labuladong/chapter01/designmydata/lfu/LFUCache.java
@@ -0,0 +1,36 @@
+package com.simple.algorithm.labuladong.chapter01.designmydata.lfu;
+
+/**
+ * 功能描述: 最近访问频率最低的元素淘汰
+ *
+ * @author: WuChengXing
+ * @create: 2022-01-03 20:01
+ **/
+public class LFUCache {
+    /**
+     * 构造容量为 capacity 的缓存
+     *
+     * @param capacity
+     */
+    public LFUCache(int capacity) {
+    }
+
+    /**
+     * 在缓存中查询 key
+     *
+     * @param key
+     * @return
+     */
+    public int get(int key) {
+        return -1;
+    }
+
+    /**
+     * 将 key 和 val 存入缓存
+     *
+     * @param key
+     * @param val
+     */
+    public void put(int key, int val) {
+    }
+}
