d586721481a4a8c330f1d2efc30aa68c58912816
wuchengxing <1449697757@qq.com>
2022-01-05 21:49:50 +0800

    [新增]：最大频率栈

---
+++ algorithm/src/main/java/com/simple/algorithm/labuladong/chapter01/designmydata/FreqStack.java
@@ -0,0 +1,11 @@
+package com.simple.algorithm.labuladong.chapter01.designmydata;
+
+/**
+ * 功能描述: 最大频率栈
+ *
+ * @author: WuChengXing
+ * @create: 2022-01-05 21:49
+ **/
+public class FreqStack {
+
+}
