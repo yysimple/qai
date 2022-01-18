daa5072a69b1865cb466b22e114624403dcc8478
wuchengxing <1449697757@qq.com>
2022-01-18 21:39:40 +0800

    [新增]：SpringBoot源码分析

--- src/main/java/com/simple/MianshiApplication.java
+++ src/main/java/com/simple/MianshiApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ImportResource;
 
 import javax.annotation.sql.DataSourceDefinition;
@@ -16,12 +17,20 @@ import javax.annotation.sql.DataSourceDefinition;
  * @author: WuChengXing
  * @create: 2021-04-07 18:54
  **/
-@SpringBootApplication
 @MapperScan(basePackages = {
         "com.simple.designpatterns.pattern23.structuretype.proxy"
 })
+@SpringBootApplication
 public class MianshiApplication {
     public static void main(String[] args) {
+        // 1. 通过SpringApplication直接调用静态run方法；
         SpringApplication.run(MianshiApplication.class, args);
+
+        // 2. 通过SpringApplicationBuilder构建出SpringApplication启动
+        new SpringApplicationBuilder(MianshiApplication.class).run(args);
+
+        // 3. 通过构造器初始化SpringApplication，然后在调用run方法
+        SpringApplication springApplication = new SpringApplication(MianshiApplication.class);
+        springApplication.run();
     }
 }
