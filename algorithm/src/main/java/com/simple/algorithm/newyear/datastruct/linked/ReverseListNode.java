68f33746e432a20d4fcd9ed875a95ff7e126242d
wuchengxing <1449697757@qq.com>
2022-02-06 15:41:55 +0800

    feat：递归反转链表

---
+++ algorithm/src/main/java/com/simple/algorithm/newyear/datastruct/linked/ReverseListNode.java
@@ -0,0 +1,54 @@
+package com.simple.algorithm.newyear.datastruct.linked;
+
+import com.simple.algorithm.newyear.datastruct.init.ListNode;
+
+/**
+ * 功能描述: 翻转链表
+ *
+ * @author: WuChengXing
+ * @create: 2022-02-06 15:22
+ **/
+public class ReverseListNode {
+
+    /**
+     * 通过遍历的方式反转链表
+     *
+     * @param root
+     * @return
+     */
+    public ListNode reverseAllNode(ListNode root) {
+        // 这里定义三个几点，用于后续的指针移动
+        ListNode curr = root, next = root, pre = null;
+        while (curr != null) {
+            // next现在对应的是头部节点
+            next = curr.next;
+            // 这里curr下一个节点为上一个节点，这样理解就是前后节点进行了交换
+            curr.next = pre;
+            // 然后交换当前节点，往后推进
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+
+    /**
+     * 以递归的形式完成链表的反转
+     *
+     * @param root
+     * @return
+     */
+    public ListNode reverseNodeDG(ListNode root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.next == null) {
+            return root;
+        }
+        ListNode last = reverseAllNode(root.next);
+        // 也是进行13交换，然就将头节点的下一个节点置空
+        root.next.next = root;
+        root.next = null;
+        return last;
+    }
+
+}
